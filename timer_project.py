756c5988fe68ac107cd31df4c15094404bdc4c51
Amy Veggeberg <amyrenae@gmail.com>
2020-12-09 04:52:56 -0500

    fixed formatting

--- timer_project.py
+++ timer_project.py
@@ -1,8 +1,6 @@
 import time
 import sys
 from tkinter import *
-from tkinter import messagebox
-
 
 # creating Tk window
 root = Tk()
@@ -10,9 +8,7 @@ root = Tk()
 # setting geometry of tk window
 root.geometry("800x800")
 
-# Using title() to display a message in
-# the dialogue box of the message in the
-# title bar.
+
 root.title("Exercise Timer")
 
 # Declaration of variables
@@ -23,32 +19,32 @@ recover_time = StringVar()
 active_time.set("00")
 recover_time.set("00")
 
-active_lbl = Label(root, text="active time", justify='center', font=("Arial", 30))
-active_lbl.place(x=10,y=5)
+active_lbl = Label(root, text="active time", justify='center', font=("Arial", 50))
+active_lbl.place(x=100,y=125)
 # active_lbl.grid(row=0, column=0)
 
 #creating the entry boxes for the interval time spans
-active_time_entry = Entry(root, width=2, font=("Arial",30,""),
+active_time_entry = Entry(root, width=2, font=("Arial",50,""),
                           textvariable=active_time, justify='center')
-active_time_entry.place(x=10,y=40)
+active_time_entry.place(x=175,y=200)
 # active_time_entry.grid(row=1, column=0)
 
-recover_lbl = Label(root, text="recover time", justify='center', font=("Arial", 30))
-recover_lbl.place(x=400,y=5)
+recover_lbl = Label(root, text="recover time", justify='center', font=("Arial", 50))
+recover_lbl.place(x=425,y=125)
 # recover_lbl.grid(row=0, column=2)
 
-recover_time_entry = Entry(root, width=2, font=("Arial",30,""),
+recover_time_entry = Entry(root, width=2, font=("Arial",50,""),
                            textvariable=recover_time, justify='center')
-recover_time_entry.place(x=400,y=40)
+recover_time_entry.place(x=500,y=200)
 # recover_time_entry.grid(row=1, column=2)
 
-
-
-timer_label = Label(root, text="00:00", justify='center', font=("Arial", 50))
-interval_countdown_label = Label(root, text="0", justify='center', font=("Arial", 200))
 interval_type_lbl = Label(root, text="",
                       justify='center',
                       font=("Arial", 100))
+
+interval_countdown_label = Label(root, text="0", justify='center', font=("Arial", 300))
+
+timer_label = Label(root, text="00:00", justify='center', font=("Arial", 50))
 
 # go_back = Button(root, text='pause', bd='5',
 #                       command= lambda: pause_timer(0, 'active', int(active_time_entry.get())),
@@ -85,21 +81,20 @@ def setup_timer():
 	recover_time_entry.place_forget()
 
 
-
 	interval_type_lbl.config(text="", bg="white")
 	# interval_type_lbl.place(x=340, y=100)
-	interval_type_lbl.pack()
+	interval_type_lbl.pack(pady=20)
 
 	interval_countdown_label.config(text="0", bg="white")
 	# interval_countdown_label.place(x=380, y=400)
-	interval_countdown_label.pack()
+	interval_countdown_label.pack(pady=50)
 
 
 	root.config(bg="white")
 
-	start_button.place(x=100, y=700)
-	pause_button.place(x=350, y=700)
-	reset_button.place(x=600, y=700)
+	start_button.place(x=100, y=600)
+	pause_button.place(x=325, y=600)
+	reset_button.place(x=580, y=600)
 
 	timer_label.config(text="00:00", bg="white")
 	# timer_label.place(x=340, y=750)
@@ -112,8 +107,8 @@ def update_timer(seconds_passed, interval, interval_time):
 
 	pause_button.config(command=lambda: pause_timer(seconds_passed, interval, interval_time))
 	phase = {'active': {'title': 'go!',
-							 'interval_duration': int(active_time_entry.get()),
-							 'color': 'green'},
+						'interval_duration': int(active_time_entry.get()),
+						'color': 'green'},
 			 'recover': {'title': 'rest',
 				  		 'interval_duration': int(recover_time_entry.get()),
 				  		 'color': 'pink'}}
@@ -160,33 +155,22 @@ def update_timer(seconds_passed, interval, interval_time):
 
 start_button = Button(root, text='start', bd='5',
                       command= lambda: start_timer(0, 'active', int(active_time_entry.get())),
-                      font=("Arial", 20))
+                      font=("Arial", 50))
 
 pause_button = Button(root, text='pause', bd='5',
                       command= lambda: pause_timer(0, 'active', int(active_time_entry.get())),
-                      font=("Arial", 20))
+                      font=("Arial", 50))
 
 reset_button = Button(root, text='reset', bd='5',
                       command=reset_timer,
-                      font=("Arial", 20))
+                      font=("Arial", 50))
 # button widget
 setup_btn = Button(root, text='Set Up Timer', bd='5',
 			 command=setup_timer,
-			 font=("Arial", 20))
-setup_btn.place(x=380, y=120)
+			 font=("Arial", 60))
+setup_btn.place(x=220, y=500)
 # setup_btn.grid(row=3, column=1)
 
-
-# timer_label = Label(root, text="00:00", justify='center', font=("Arial", 50))
-# timer_label.place(x=110, y=150)
-
-# interval_countdown_label = Label(root, text="0", justify='center', font=("Arial", 32))
-# interval_countdown_label.place(x=70, y=250)
-
-# interval_type_lbl = Label(root, text="",
-#                           justify='center',
-#                           font=("Arial", 100))
-# interval_type_lbl.place(x=60, y=20)
 
 # infinite loop which is required to
 # run tkinter program infinitely
